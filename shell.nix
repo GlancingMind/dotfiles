665d5fd66f87eb905d96f061e6c5d1584125153f
Sascha <GlancingMind@outlook.com>
2020-09-14 20:49:13 +0200

    fix updating of packages - besides home-manager and niv, no other package seems to be updated on updated channel version

--- shell.nix
+++ shell.nix
@@ -1,11 +1,19 @@
 let
-  pkgs = import ./nix;
-in
-pkgs.mkShell {
+  sources = import ./nix/sources.nix;
+  nixpkgs = sources.nixpkgs;
+  pkgs = import nixpkgs {};
+in pkgs.stdenv.mkDerivation {
+
+  name = "home-manager-shell";
+
   buildInputs = [
     pkgs.niv
-    pkgs.home-manager
+    (import sources.home-manager {inherit pkgs;}).home-manager
     pkgs.nixops
     pkgs.gnumake
   ];
+
+  shellHocks = ''
+    export NIX_PATH="nixpkgs=${nixpkgs}";
+  '';
 }
