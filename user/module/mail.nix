c8516ab35811b7065e380fd41a9526eb4f859168
Sascha <GlancingMind@outlook.com>
2020-10-30 17:14:48 +0100

    improved mail config generation

--- user/module/mail.nix
+++ user/module/mail.nix
@@ -1,151 +1,140 @@
 { config, pkgs, lib, ... }:
+with lib;
 let
-  pws-dir = "${config.programs.password-store.settings.PASSWORD_STORE_DIR}";
-  pass = (import ../../nix-plugins).nix-plugins.pass;
-  pass-name = expandedPath:
-    lib.strings.removeSuffix ".gpg" (
-      lib.strings.removePrefix (pws-dir+"/") expandedPath);
-  extractAppPassword = "sed -n 's/^.*app-password:\\s*\\(\\S*\\).*$/\\1/p'";
-  template = import ./test.nix {};
+  pass = (import ../../nix-plugins).pass;
 
-  webDeAccounts = builtins.listToAttrs (template.expand {
-    path="${pws-dir}/personal/email/web.de";
-    do = {expandedPath}: {
-      name = pass-name expandedPath;
-      value = rec {
-        primary = "92747cf9026d18d1d133fcde0b64a2904c1ec1f0" == builtins.hashString "sha1" address;
-        address = builtins.head (pass.decrypt.lookup "email: +(.*)" (pass-name expandedPath));
-        userName = address;
-        passwordCommand="pass show ${pass-name expandedPath}";
+  inStorePath = account: "personal/email/${account}";
 
-        folders = {
-          drafts = "Entwurf";
-          inbox = "Inbox";
-          sent = "Gesendet";
-          trash = "Papierkorb";
-        };
+  schema = {
+    email = "email: +(.*)";
+    login = "login: +(.*)";
+  };
 
-        imap = {
-          host = "imap.web.de";
-          port = 993;
-          tls = {
-            enable = true;
-            useStartTls = false;
-          };
-        };
+  expandConfig = domain: config: let
+    accs = builtins.filter (hasSuffix domain) (pass.files "personal/email");
+    configs = map (acc: attrsets.nameValuePair acc (config acc)) accs;
+  in builtins.listToAttrs configs;
 
-        mbsync = {
+  webDeAccounts = let
+    config = account: rec {
+      primary = "92747cf9026d18d1d133fcde0b64a2904c1ec1f0" == builtins.hashString "sha1" address;
+      address = pass.decrypt.lookupFirst schema.email (inStorePath account);
+      userName = address;
+      passwordCommand= "pass show ${inStorePath account}";
+
+      folders = {
+        inbox = "INBOX";
+        sent = "Gesendet";
+        drafts = "Entwurf";
+        trash = "Papierkorb";
+      };
+
+      imap = {
+        host = "imap.web.de";
+        port = 993;
+        tls = {
           enable = true;
-          create = "maildir";
+          useStartTls = false;
         };
       };
-    };
-  });
 
-  outlookAccounts = builtins.listToAttrs (template.expand {
-    path="${pws-dir}/personal/email/outlook.com";
-    do = {expandedPath}: {
-      name = pass-name expandedPath;
-      value = let
-      in rec {
-        address = builtins.head (pass.decrypt.lookup "email: +(.*)" (pass-name expandedPath));
-        userName = address;
-        passwordCommand= "pass show ${pass-name expandedPath} | awk '/app-password:/ {print $2}'";
-
-        folders = {
-          drafts = "Drafts";
-          inbox = "Inbox";
-          sent = "Sent";
-          trash = "Deleted"; #
-        };
-
-        imap = {
-          host = "outlook.office365.com";
-          port = 993;
-          tls = {
-            enable = true;
-            useStartTls = false;
-          };
-        };
-
-        mbsync = {
-          enable = true;
-          create = "maildir";
-        };
+      mbsync = {
+        enable = true;
+        create = "maildir";
       };
     };
-  });
+  in expandConfig "@web.de" config;
 
-  #TODO
-  # load mbsync from unstable channel and test in shell, if problem is resolved
-  yahooAccounts = builtins.listToAttrs (template.expand {
-    path="${pws-dir}/personal/email/yahoo.com";
-    do = {expandedPath}: {
-      name = pass-name expandedPath;
-      value = rec {
-        address = builtins.head (pass.decrypt.lookup "email: +(.*)" (pass-name expandedPath));
-        userName = address;
-        passwordCommand= "pass show ${pass-name expandedPath} | awk '/app-password:/ {print $2}'";
+  outlookAccounts = let
+    config = account: rec {
+      address = pass.decrypt.lookupFirst schema.email (inStorePath account);
+      userName = address;
+      passwordCommand= "pass show ${inStorePath account} | awk '/app-password:/ {print $2}'";
 
-        folders = {
-          drafts = "Draft"; #
-          inbox = "Inbox";
-          sent = "Sent";
-          trash = "Trash";
-        };
+      folders = {
+        inbox = "INBOX";
+        sent = "Sent";
+        drafts = "Drafts";
+        trash = "Deleted";
+      };
 
-        imap = {
-          host = "imap.mail.yahoo.com";
-          port = 993;
-          tls = {
-            enable = true;
-            useStartTls = false;
-          };
-        };
-
-        mbsync = {
+      imap = {
+        host = "outlook.office365.com";
+        port = 993;
+        tls = {
           enable = true;
-          create = "maildir";
+          useStartTls = false;
         };
       };
-    };
-  });
 
-  thMail = builtins.listToAttrs (template.expand {
-    path="${pws-dir}/personal/email/mni.thm.de";
-    do = {expandedPath}: {
-      name = pass-name expandedPath;
-      value = rec {
-        address = builtins.head (pass.decrypt.lookup "email: +(.*)" (pass-name expandedPath));
-        userName = builtins.head (pass.decrypt.lookup "login: +(.*)" (pass-name expandedPath));
-        passwordCommand="pass show ${pass-name expandedPath}";
-
-        folders = {
-          drafts = "Drafts";#
-          inbox = "Inbox";  #
-          sent = "Sent";    #
-          trash = "Trash";  #
-        };
-
-        imap = {
-          host = "mailgate.thm.de";
-          port = 993;
-          tls = {
-            enable = true;
-            useStartTls = false;
-          };
-        };
-
-        mbsync = {
-          enable = true;
-          create = "maildir";
-        };
+      mbsync = {
+        enable = true;
+        create = "maildir";
       };
     };
-  });
+  in expandConfig "@outlook.com" config;
+
+  yahooAccounts = let
+    config = account: rec {
+      address = pass.decrypt.lookupFirst schema.email (inStorePath account);
+      userName = address;
+      passwordCommand= "pass show ${inStorePath account} | awk '/app-password:/ {print $2}'";
+
+      folders = {
+        inbox = "INBOX";
+        sent = "Sent";
+        drafts = "Draft";
+        trash = "Trash";
+      };
+
+      imap = {
+        host = "imap.mail.yahoo.com";
+        port = 993;
+        tls = {
+          enable = true;
+          useStartTls = false;
+        };
+      };
+
+      mbsync = {
+        enable = true;
+        create = "maildir";
+      };
+    };
+  in expandConfig "@yahoo.de" config;
+
+  thAccounts = let
+    config = account: rec {
+      address = pass.decrypt.lookupFirst schema.email (inStorePath account);
+      userName = pass.decrypt.lookupFirst schema.login (inStorePath account);
+      passwordCommand= "pass show ${inStorePath account}";
+
+      folders = {
+        inbox = "INBOX";
+        sent = "INBOX/Sent";
+        drafts = "INBOX/Drafts";
+        trash = "INBOX/Trash";
+      };
+
+      imap = {
+        host = "mailgate.thm.de";
+        port = 993;
+        tls = {
+          enable = true;
+          useStartTls = false;
+        };
+      };
+
+      mbsync = {
+        enable = true;
+        create = "maildir";
+      };
+    };
+  in expandConfig "@mni.thm.de" config;
 in
 {
   programs.mbsync.enable = true;
 
-  accounts.email.accounts = outlookAccounts // yahooAccounts // webDeAccounts // thMail;
+  accounts.email.accounts = webDeAccounts // yahooAccounts
+    // outlookAccounts // thAccounts;
 }
