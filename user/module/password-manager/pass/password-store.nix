db573c8c9ee68e395727d61ee19225cc41c808a2
Sascha <GlancingMind@outlook.com>
2021-01-01 17:06:30 +0100

    use console pinentry gpg

--- user/module/password-manager/pass/password-store.nix
+++ user/module/password-manager/pass/password-store.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    pinentry #needed for gpg
+    pinentry        #needed for gpg
+    pinentry-curses #needed for gpg
+    gopass
   ];
 
   programs.gpg.enable = true;
@@ -9,6 +11,13 @@
     enable = true;
     defaultCacheTtl = 300;
     maxCacheTtl = 600;
+    pinentryFlavor = "curses";
+    grabKeyboardAndMouse = true;
+
+    extraConfig = ''
+      ignore-cache-for-signing
+      no-allow-external-cache
+    '';
   };
 
   programs.password-store = {
